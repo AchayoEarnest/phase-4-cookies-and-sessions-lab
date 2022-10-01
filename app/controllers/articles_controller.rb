50f5ed8ec25d7d72fc8e7278b7e516cea752eefe
Earnest Achayo <earnest.achayo@student.moringaschool.com>
2022-10-01 12:20:36 +0300

    commit tree c806ca9c77550bf5693e4c2e591c2aa4bcfe7c33

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -15,7 +15,6 @@ class ArticlesController < ApplicationController
         render json: article
     else
       render json: { error: "Maximum pageview limit reached" }, status: :unauthorized
-      # render json: {error: "Maximum pageview limit reached" }, status: :unauthorized
     end
   end
 
